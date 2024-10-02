0b3fa04eb27454157172b74d0f52188c32f298de
allison-nalesnik <107144627+allison-nalesnik@users.noreply.github.com>
2024-10-02 12:01:46 -0400

    Made global packages.

--- scripts/01-process_shapes.R
+++ scripts/01-process_shapes.R
@@ -7,34 +7,36 @@ library(tidyverse)
 
 # data --------------------------------------------------------------------
 
-# #unzip shapefiles in shapefile folder. Keep the origin names
+# #unzip shapefiles in shapefile folder. Keep the original names
 
-# usa <- 
-#   read_sf('shapefiles/cb_2018_us_state_500k.shp') |>
-#   st_make_valid() |> 
-#   janitor::clean_names()
+## This script is taking the originally downloaded files and generating global packages
 
-# usa |> 
-#   write_sf('shapefiles/usa.gpkg')
-# 
-# plot(st_geometry(usa))
+usa <-
+  read_sf('shapefiles/cb_2018_us_state_500k.shp') |>
+  st_make_valid() |>
+  janitor::clean_names()
 
-# counties <-
-#   read_sf('shapefiles/cb_2018_us_county_500k.shp') |>
-#    st_make_valid() |>
-#    janitor::clean_names()
-# 
-# plot(st_geometry(counties))
-# 
-# counties |>
-#   write_sf('shapefiles/counties.gpkg')
+usa |>
+  write_sf('shapefiles/usa.gpkg')
 
-# world <-
-#   read_sf('shapefiles/world.geojson') |>
-#    st_make_valid() |>
-#    janitor::clean_names()
-# 
-# plot(st_geometry(world))
-# 
-# world |>
-#   write_sf('shapefiles/world.gpkg')
+plot(st_geometry(usa))
+
+counties <-
+  read_sf('shapefiles/cb_2018_us_county_500k.shp') |>
+   st_make_valid() |>
+   janitor::clean_names()
+
+plot(st_geometry(counties))
+
+counties |>
+  write_sf('shapefiles/counties.gpkg')
+
+world <-
+   read_sf('shapefiles/world.geojson') |>
+   st_make_valid() |>
+   janitor::clean_names()
+
+plot(st_geometry(world))
+
+world |>
+  write_sf('shapefiles/world.gpkg')
